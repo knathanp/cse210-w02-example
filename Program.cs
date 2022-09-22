9846228f8bc1c8d14d5a3a8f4c4dc0d2e309ef87
Nate Phillips <knathanp@gmail.com>
2022-09-22 10:52:32 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            names.Add("Bob");
+            names.Add("Bill");
+            names.Add("Sue");
+
+            names.ForEach(name => Console.Write(name + " "));
+
+
+            string aString = "a";
+            List<string> stuff = new List<string> { aString, "b", "c", "d" };
+
+            char aSingleCharacter = 'a';
+            Console.WriteLine($"This is a single character: {aSingleCharacter}");
+
+            int x = 5;
+            int y = 7;
+            int result = Adder(x, y); // 12
+
+            int result2 = Adder(10, 46); // 56
+
+            Console.WriteLine($"{x} + {y} = {result}");
+
+
+
+        }
+
+        static int Adder(int a, int b)
+        {
+            return a + b;
+        }
+
+
+    }
+}
